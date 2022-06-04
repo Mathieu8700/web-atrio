ccd17593f6d0f9e1643f423318162430c3ed7299
Mathieu8700 <106870674+Mathieu8700@users.noreply.github.com>
2022-06-04 14:38:15 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,85 @@
+<?
+//ouverture de session
+session_start();
+
+//Les erreur PHP
+error_reporting(E_ERROR | E_WARNING | E_PARSE);
+ini_set('display_errors', '2');
+ini_set('upload_max_filesize', '100M');
+//le time zone
+date_default_timezone_set('Europe/Paris');
+//Les classes & LIBRAIRIE
+define('CLASS_DIR',  $_SERVER['DOCUMENT_ROOT'].'/class/');
+set_include_path(get_include_path().PATH_SEPARATOR.CLASS_DIR);
+spl_autoload_extensions('.class.php');
+spl_autoload_register();
+
+//connexion a la base de donne MYSQL BDD PHP OBJECT (CLASS DB)
+//Server / utilisateur / pass / Nom de la base / prefix de la table (securité)
+$db = new db('localhost','noah_user','y996Hcc_','bdd_noah','WEB');
+
+//si nous ne postons pas la page ont detruit la session (rechargement de page)
+//et donc reposter un autre formulaire
+if(!isset($_POST['envoyer'])):
+	unset($_SESSION['envoi']);
+					endif;
+
+//si nous cliquons sur le bouton visualise
+if(isset($_GET['visualiser'])):
+
+//liste des Personne trié par Nom alphabetiquement
+$liste = $db->find('*',array('order' => 'nom ASC'),'Personne');
+//si la liste nest pas vide
+if($liste):
+foreach($liste as $lst){
+
+echo '<ul><li>'.utf8_encode($lst['nom']).' '.utf8_encode($lst['prenom']).' '.date('d/m/Y',strtotime($lst['naissance'])).'</li></ul>';	
+						
+						}	
+			endif;
+			
+			  endif;
+
+
+
+//declaration de la variable contenant le message erreur
+$erreur = '';
+
+//si nous postons le formulaire
+if(isset($_POST['envoyer']) && !isset($_SESSION['envoi'])):
+	//les postes
+	$nom    = $_POST['nom'];
+	$prenom = $_POST['prenom'];
+	$date   = $_POST['date'];
+	
+	if($date):
+	//Verification de l'age de l'utilisateur
+	$today = date('Y-m-d');
+	$dateDifference = abs(strtotime($today) - strtotime($date));
+	//nombre anné differences
+	$years  = floor($dateDifference / (365 * 60 * 60 * 24));
+	
+	
+	//si inferieur a 150 ans
+	if($years >= 150):
+	$erreur = '<div class="error">Erreur il ne peux y avoir de personne agéee de plus de 150 ans</div>';	
+				endif;
+				
+	else:
+		
+		$erreur = '<div class="error">Date de naissance obligatoire</div>';
+		
+	endif;
+	
+	//si nous n'avons pas d'erreur ont enregistre en base
+	$db->add(array('nom' => $nom,'prenom' => $prenom,'naissance' => $date),'Personne');	
+	
+	//pour ne pas poster plusieurs fois le formulaires
+	$_SESSION['envoi'] = 1;			
+	
+						endif;
+
+?>
 <!doctype html>
 <html lang="en">
 <head>
@@ -6,12 +88,13 @@
 	<title>Title</title>
 	<style>
 		.error{ color:#fff; background: red; padding: 5px; }
+		.button-wrapper{ display: flex; justify-content: space-between; }
 	</style>
 </head>
 <body>
-	<form>
+	<form method="post" action="#">
 		<fieldset>
-			<div class="error">Erreur il ne peux y avoir de personne agéee de plus de 150 ans</div>
+			<? echo $erreur; ?>
 			<div>
 				<label for="nom">Nom</label>
 				<input type="text" value="" name="nom" id="nom"/>
@@ -26,7 +109,14 @@
 				
 			</div>
 		</fieldset>
+		<div class="button-wrapper">
 		<button type="submit" name="envoyer">Valider</button>
+		<? if(!isset($_GET['visualiser'])): ?>
+		<a href="m.php?visualiser" target="parent">Visualiser</a>
+		<? else: ?>
+		<a href="m.php" target="parent">Retour</a>
+		<? endif; ?>
+		</div>
 	</form>
 	
 </body>
