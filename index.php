867507a3eaae376f8f82e4d819c80d9886cc4815
Mathieu8700 <106870674+Mathieu8700@users.noreply.github.com>
2022-06-04 14:54:18 +0200

    Update index.php

--- index.php
+++ index.php
@@ -16,7 +16,8 @@ spl_autoload_register();
 
 //connexion a la base de donne MYSQL BDD PHP OBJECT (CLASS DB)
 //Server / utilisateur / pass / Nom de la base / prefix de la table (securité)
-$db = new db('localhost','noah_user','y996Hcc_','bdd_noah','WEB');
+$db       = new db('localhost','noah_user','y996Hcc_','bdd_noah','WEB');
+$perssone = new personne();
 
 //si nous ne postons pas la page ont detruit la session (rechargement de page)
 //et donc reposter un autre formulaire
@@ -51,31 +52,9 @@ if(isset($_POST['envoyer']) && !isset($_SESSION['envoi'])):
 	$nom    = $_POST['nom'];
 	$prenom = $_POST['prenom'];
 	$date   = $_POST['date'];
+	//appel de la fonction qui test et enregistre dans la base
+	$erreur = $perssone::_postFormulaire($nom,$prenom,$date);
 	
-	if($date):
-	//Verification de l'age de l'utilisateur
-	$today = date('Y-m-d');
-	$dateDifference = abs(strtotime($today) - strtotime($date));
-	//nombre anné differences
-	$years  = floor($dateDifference / (365 * 60 * 60 * 24));
-	
-	
-	//si inferieur a 150 ans
-	if($years >= 150):
-	$erreur = '<div class="error">Erreur il ne peux y avoir de personne agéee de plus de 150 ans</div>';	
-				endif;
-				
-	else:
-		
-		$erreur = '<div class="error">Date de naissance obligatoire</div>';
-		
-	endif;
-	
-	//si nous n'avons pas d'erreur ont enregistre en base
-	$db->add(array('nom' => $nom,'prenom' => $prenom,'naissance' => $date),'Personne');	
-	
-	//pour ne pas poster plusieurs fois le formulaires
-	$_SESSION['envoi'] = 1;			
 	
 						endif;
 
